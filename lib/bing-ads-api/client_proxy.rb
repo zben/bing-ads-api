6a22dfb148faef7c64d5699a8a341ada22c77ee5
Emily Winck <ewinck@neboagency.com>
2017-10-18 14:41:40 -0400

    Update other references from v9 to v11

--- lib/bing-ads-api/client_proxy.rb
+++ lib/bing-ads-api/client_proxy.rb
@@ -26,7 +26,7 @@ module BingAdsApi
 	class ClientProxy
 		
 		# Public : Namespace para atributos bing. Hace referencia a la versión de API usada 
-		NAMESPACE = :v9
+		NAMESPACE = :v11
 
 		# Public : Case empleado los nombres de atributos en los XML 
 		KEYS_CASE = :camelcase
